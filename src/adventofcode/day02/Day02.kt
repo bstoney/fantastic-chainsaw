77413aca2b9d1efa38df746de5044483fe3ff373
Brad Stoney <stoneby@amazon.com>
2022-12-04 14:46:42 -0800

    Day 02 - part 2

--- src/adventofcode/day02/Day02.kt
+++ src/adventofcode/day02/Day02.kt
@@ -6,27 +6,47 @@ fun main() {
     Solution.solve()
 }
 
-object Solution : AdventOfCodeSolution() {
+object Solution : AdventOfCodeSolution(true) {
     fun solve() {
-        solve(2, 15)
+        solve(2, 15, 12)
     }
 
     override fun part1(input: List<String>): Int {
-        return getRounds(input).sumOf { it.roundScore() }
+        return getRounds(input) { _: Play, playerInput: String ->
+            when (playerInput) {
+                "X" -> Play.A
+                "Y" -> Play.B
+                "Z" -> Play.C
+                else -> throw IllegalArgumentException("Illegal player input: $playerInput")
+            }
+        }
+            .sumOf { it.roundScore() }
     }
 
-    private fun getRounds(input: List<String>): List<Round> = input
+    override fun part2(input: List<String>): Int? {
+        return getRounds(input) { opponentPlay: Play, playerInput: String ->
+            when (playerInput) {
+                "X" -> opponentPlay.defeats()
+                "Y" -> opponentPlay
+                "Z" -> opponentPlay.defeatedBy()
+                else -> throw IllegalArgumentException("Illegal player input: $playerInput")
+            }
+        }
+            .sumOf { it.roundScore() }
+    }
+
+    private fun getRounds(
+        input: List<String>,
+        playerMove: (opponentInput: Play, playerInput: String) -> Play
+    ): List<Round> = input
         .filter { it.length == 3 }
-        .map(::getRound)
-
-    private fun getRound(input: String): Round {
-        val plays = input.split(" ")
-            .map(Play::valueOf)
-
-        val round = Round(plays[0], plays[1])
-        debug("$round -> ${round.opponent.move} vs ${round.player.move}: ${round.roundResult()} + ${round.player.move} = ${round.roundScore()}")
-        return round
-    }
+        .map { it.split(" ") }
+        .map { (opponentInput, playerInput) ->
+            val opponentPlay = Play.valueOf(opponentInput)
+            val round = Round(opponentPlay, playerMove(opponentPlay, playerInput))
+            debug("${round.opponent.move} ($opponentInput) vs ${round.player.move} ($playerInput): ${round.roundResult()} + ${round.player.move} = ${round.roundScore()}")
+            round
+        }
 
     private data class Round(val opponent: Play, val player: Play) {
         fun roundResult() = opponent.playAgainst(player)
@@ -37,24 +57,34 @@ object Solution : AdventOfCodeSolution() {
     private enum class RoundResult(val score: Int) {
         Win(6),
         Draw(3),
-        Lose(0);
+        Lose(0),
+        ;
     }
 
-    private enum class Play(val move: Move, private val defeats: Move) {
-        A(Move.Rock, Move.Scissors),
-        B(Move.Paper, Move.Rock),
-        C(Move.Scissors, Move.Paper),
-        X(Move.Rock, Move.Scissors),
-        Y(Move.Paper, Move.Rock),
-        Z(Move.Scissors, Move.Paper),
+    private enum class Play(val move: Move) {
+        A(Move.Rock),
+        B(Move.Paper),
+        C(Move.Scissors),
         ;
 
         fun playAgainst(play: Play): RoundResult =
             when (play.move) {
                 this.move -> RoundResult.Draw
-                defeats -> RoundResult.Lose
+                defeats().move -> RoundResult.Lose
                 else -> RoundResult.Win
             }
+
+        fun defeats(): Play = when (move) {
+            Move.Rock -> C
+            Move.Paper -> A
+            Move.Scissors -> B
+        }
+
+        fun defeatedBy(): Play = when (move) {
+            Move.Rock -> B
+            Move.Paper -> C
+            Move.Scissors -> A
+        }
     }
 
     private enum class Move(val score: Int) {
