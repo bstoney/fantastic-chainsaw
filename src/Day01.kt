9b8c3539ded51fb131b698526ff23333b868994f
Brad Stoney <stoneby@amazon.com>
2022-12-04 11:05:54 -0800

    Day 01 - part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,6 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return Solve.getMaxCarriedCalories(input)
     }
 
     fun part2(input: List<String>): Int {
@@ -9,9 +9,29 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testPart1 = part1(testInput)
+    println(testPart1)
+    check(testPart1 == 24000)
 
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
+}
+
+object Solve {
+    private fun getElvesCalories(input: List<String>): Array<List<Int>> {
+        val nextElfItems = input.takeWhile { it.isNotEmpty() }.map { it.toInt() }
+        if (nextElfItems.size < input.size) {
+            return arrayOf(
+                nextElfItems,
+                * getElvesCalories(input.subList(nextElfItems.size + 1, input.size))
+            )
+        }
+
+        return arrayOf(nextElfItems)
+    }
+
+    fun getMaxCarriedCalories(input: List<String>): Int {
+        return getElvesCalories(input).maxOf { it.sum() }
+    }
 }
