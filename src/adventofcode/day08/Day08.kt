f18dff8251b1d30f2b7c8b267f31345895088a1c
Brad Stoney <stoneby@amazon.com>
2022-12-07 22:58:35 -0800

    Day 08 - part 2

--- src/adventofcode/day08/Day08.kt
+++ src/adventofcode/day08/Day08.kt
@@ -17,13 +17,13 @@ object Solution : AdventOfCodeSolution<Int>() {
     }
 
     override fun part2(input: List<String>): Int? {
-        return super.part2(input)
+        return getTrees(input).maxOfOrNull { it.scenicScore }
     }
 
     private fun getTrees(input: List<String>): List<Tree> {
         val trees = input.mapIndexed { r, row ->
             row.map { char -> char.code - 0x30 }
-                .mapIndexed { c, height -> Tree(r, c, height) }
+                .mapIndexed { c, height -> UnexploredTree(r, c, height) }
         }
 
         val maxR = trees.lastIndex
@@ -32,22 +32,11 @@ object Solution : AdventOfCodeSolution<Int>() {
         return trees.flatten()
             .map { tree ->
                 with(tree) {
-                    when {
-                        (r > 0 && c > 0 && c < maxC && r < maxR) -> {
-                            if ((r - 1 downTo 0).map { trees[it][c] }.any { isBlockedBy(it) }
-                                && (r + 1 .. maxR).map { trees[it][c] }.any { isBlockedBy(it) }
-                                && (c - 1 downTo 0).map { trees[r][it] }.any { isBlockedBy(it) }
-                                && (c + 1..maxC).map { trees[r][it] }.any { isBlockedBy(it) }) {
-                                tree.withVisibility(TreeVisibility.Hidden)
-                            } else {
-                                tree.withVisibility(TreeVisibility.Visible)
-                            }
-                        }
-
-                        else -> {
-                            tree.withVisibility(TreeVisibility.Visible)
-                        }
-                    }
+                    val treeNorth = (r - 1 downTo 0).map { trees[it][c] }.find { isBlockedBy(it) } ?: Edge(0, c)
+                    val treeSouth = (r + 1..maxR).map { trees[it][c] }.find { isBlockedBy(it) } ?: Edge(maxR, c)
+                    val treeWest = (c - 1 downTo 0).map { trees[r][it] }.find { isBlockedBy(it) } ?: Edge(r, 0)
+                    val treeEast = (c + 1..maxC).map { trees[r][it] }.find { isBlockedBy(it) } ?: Edge(r, maxC)
+                    tree.viewTo(treeNorth, treeEast, treeSouth, treeWest)
                 }
             }
             .peek { debug("tree ${it.r},${it.c} ${it.height} is ${it.visibility}") }
@@ -60,8 +49,18 @@ object Solution : AdventOfCodeSolution<Int>() {
         Unknown,
     }
 
-    data class Tree(val r: Int, val c: Int, val height: Int, val visibility: TreeVisibility = TreeVisibility.Unknown) {
-        fun withVisibility(visibility: TreeVisibility): Tree = Tree(r, c, height, visibility)
+    interface GridLocation {
+        val r: Int
+        val c: Int
+    }
+
+    data class Edge(override val r: Int, override val c: Int) : GridLocation
+
+    interface Tree : GridLocation {
+        val height: Int
+        val visibility: TreeVisibility
+        val scenicScore: Int
+
         fun isBlockedBy(other: Tree): Boolean {
             if (other.height >= height) {
                 debug("tree: $r,$c $height")
@@ -71,5 +70,44 @@ object Solution : AdventOfCodeSolution<Int>() {
 
             return false
         }
+    }
+
+    data class UnexploredTree(override val r: Int, override val c: Int, override val height: Int) : Tree {
+        override val visibility = TreeVisibility.Unknown
+
+        override val scenicScore: Int
+            get() = throw IllegalStateException("Unexplored")
+
+        fun viewTo(n: GridLocation, e: GridLocation, s: GridLocation, w: GridLocation) =
+            ExploredTree(r, c, height, ViewTo(n, e, s, w))
+    }
+
+    data class ExploredTree(
+        override val r: Int,
+        override val c: Int,
+        override val height: Int,
+        val view: ViewTo
+    ) : Tree {
+        override val visibility: TreeVisibility by lazy {
+            if (view.any { it is Edge }) {
+                TreeVisibility.Visible
+            } else {
+                TreeVisibility.Hidden
+            }
+        }
+
+        override val scenicScore: Int by lazy {
+            val viewNorth = r - view.n.r
+            val viewEast = view.e.c - c
+            val viewSouth = view.s.r - r
+            val viewWest = c - view.w.c
+
+            viewNorth * viewEast * viewSouth * viewWest
+        }
+    }
+
+    data class ViewTo(val n: GridLocation, val e: GridLocation, val s: GridLocation, val w: GridLocation) :
+        Iterable<GridLocation> {
+        override fun iterator(): Iterator<GridLocation> = listOf(n, e, s, w).iterator()
     }
 }
